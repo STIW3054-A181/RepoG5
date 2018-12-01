84f68e931d55fb8aef46f8b5b96098687c2cece0
Koon Fung Yee <43363198+KoonFungYee@users.noreply.github.com>
2018-12-02 00:46:27 +0800

    Add files via upload

---
+++ CountWords/src/main/java/KoonFungYee/Row.java
@@ -0,0 +1,14 @@
+package KoonFungYee;
+
+public class Row {
+    private final String row;
+
+    public Row(String row) {
+        this.row = row;
+    }
+
+    public String getRow() {
+        return row;
+    }
+
+}
